5c11dc81e4b1fa6afd4e9ca9414c0cb1507224b1
satyarth misra <misrasatyarth@gmail.com>
2019-12-16 14:43:28 +0530

    Adding console logging along changes to RestaurantParam class and restaurant controller to accept the data as query strings

--- RestaurantsDataAccessLayer/DbContext/RestaurantsDbContext.cs
+++ RestaurantsDataAccessLayer/DbContext/RestaurantsDbContext.cs
@@ -1,19 +1,34 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
 using RestaurantsDomainLayer.Entities;
 
 namespace RestaurantsDataAccessLayer.DbContext
 {
     public class RestaurantsDbContext : IdentityDbContext<ApplicationUser>
     {
+
+        public static readonly LoggerFactory MyLoggerFactory =
+            new LoggerFactory(new[]
+            {
+                new ConsoleLoggerProvider((category,level)=> category == DbLoggerCategory.Database.Command.Name
+                                                             && level == LogLevel.Information, true)
+            });
+
         public RestaurantsDbContext(DbContextOptions<RestaurantsDbContext> dbContextOptions) : base(dbContextOptions)
         {
 
         }
 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseLoggerFactory(MyLoggerFactory);
+        }
+
         public DbSet<Restaurant> Restaurants { get; set; }
         public DbSet<FoodItem> FoodItems { get; set; }
         public DbSet<Address> Addresses { get; set; }
-
     }
 }
